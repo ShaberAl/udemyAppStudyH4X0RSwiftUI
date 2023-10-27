4e015be46fbd2c72bd91507602160606b437ec83
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-27 11:12:19 +0500

    Initial Commit

---
+++ udemyAppStudyH4X0RSwiftUI/udemyAppStudyH4X0RSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemyAppStudyH4X0RSwiftUIApp.swift
+//  udemyAppStudyH4X0RSwiftUI
+//
+//  Created by MacOS on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct udemyAppStudyH4X0RSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
